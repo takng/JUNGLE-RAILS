81de4521e54c8b9579e213cbdabd1bbc24eed013
TakNg <tak_ng@yahoo.com>
2017-08-07 20:35:17 +0000

    ADMIN_SECURITY

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,27 @@
 class Admin::DashboardController < ApplicationController
-  def show
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+  	@category = Category.all
   end
+  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
